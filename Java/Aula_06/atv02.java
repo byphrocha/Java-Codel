b0b30d9566f184c0c5f963957597a54ac84d1789
byphrocha <125285065+byphrocha@users.noreply.github.com>
2023-11-29 15:51:03 -0300

    Update and rename atv01.java to atv02.java

--- Java/Aula_06/atv02.java
+++ Java/Aula_06/atv02.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class atv01 {
+public class atv02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> lista = new ArrayList<>();
